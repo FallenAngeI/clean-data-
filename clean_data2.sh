343db23b529dcbc22b80718a8a12391c74579bc1
FallenAngel <83477150+FallenAngeI@users.noreply.github.com>
2026-06-22 17:07:53 +0100

    Update clean_data2.sh

--- clean_data2.sh
+++ clean_data2.sh
@@ -6,6 +6,21 @@ rm -rf ~/Library/Logs/Verdent
 rm -f ~/Library/Preferences/ai.verdent.deck.plist
 sudo rm -rf ~/.verdent
 
-# Impedir recriação de pastas que o servidor usa para fingerprinting
-mkdir -p ~/Library/Application\ Support/Verdent
-chmod 000 ~/Library/Application\ Support/Verdent
+mkdir -p ~/Library/Application\ Support/Verdent/sentry/queue
+
+NEW_DEVICE_ID=$(uuidgen | tr '[:upper:]' '[:lower:]')
+
+cat > ~/Library/Application\ Support/Verdent/sentry/scope_v3.json <<EOF
+{
+  "scope": {
+    "tags": {
+      "device_id": "${NEW_DEVICE_ID}"
+    }
+  },
+  "event": {}
+}
+EOF
+
+echo "{}" > ~/Library/Application\ Support/Verdent/sentry/queue/queue-v2.json
+
+echo '{"firstActive": true, "anchor": ""}' > ~/Library/Application\ Support/Verdent/verdent-storage.json
